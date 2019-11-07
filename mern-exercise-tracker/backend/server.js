4a8d80526c910a79347c607e98aa9af26dfb88b0
John Hudson <johnhudson@JohnHudsonsAir.attlocal.net>
2019-11-07 17:35:29 -0500

    MongoDB connected

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -12,7 +12,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// MongoDB uri is where DB is stored
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+ 
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
 // Starts server to help with listening
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});  
+});
